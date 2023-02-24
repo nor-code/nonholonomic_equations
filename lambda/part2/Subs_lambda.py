50563691bbc9198063db3839531e88c16ab27700
Нор Никита <nikita.nor@gazprombank.ru>
2023-02-24 19:38:13 +0300

    refactoring subs lambda in equations

--- lambda/part2/Subs_lambda.py
+++ lambda/part2/Subs_lambda.py
@@ -15,9 +15,21 @@ t1 = time.time()
 def subs_lambda_to_equation(eq, i):
     global λ_1_expression, λ_2_expression, λ_3_expression, λ_4_expression
     eq_top, _ = fraction(together(eq.subs(λ_1, λ_1_expression)))
+    eq_top = remove_required_and_above_smallness_from_expression(expand(eq_top, deep=True), order=5)
+    print(str(i), " part 1 finished")
+
     eq_top, _ = fraction(together(eq_top.subs(λ_2, λ_2_expression)))
+    eq_top = remove_required_and_above_smallness_from_expression(expand(eq_top, deep=True), order=5)
+    print(str(i), " part 2 finished")
+
     eq_top, _ = fraction(together(eq_top.subs(λ_3, λ_3_expression)))
+    eq_top = remove_required_and_above_smallness_from_expression(expand(eq_top, deep=True), order=5)
+    print(str(i), " part 3 finished")
+
     eq_top, _ = fraction(together(eq_top.subs(λ_4, λ_4_expression)))
+    eq_top = remove_required_and_above_smallness_from_expression(expand(eq_top, deep=True), order=5)
+    print(str(i), " part 4 finished")
+
     with open("../../lambda/part2/eq_" + str(i) + "_without_lambda.txt", 'w') as out:
         out.write(transform_to_simpy(str(eq_top)))
 
@@ -42,4 +54,4 @@ for task in tasks:
 
 t2 = time.time()
 
-print("finished substitute lambdas in equations. total time = %.2f " % ((t2 - t1) / 60))
+print("finished substitute lambdas in 1,2,3,4,5,7 equations. total time = %.2f " % ((t2 - t1) / 60))
