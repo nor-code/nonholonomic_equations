e0e78811a7af9b91d0c2ea3ece5f89b132ee7f62
Нор Никита <nikita.nor@gazprombank.ru>
2023-03-03 16:10:01 +0300

    for lambda expression change source dynamic equations change order from 5 to 2

--- lambda/part1/Resolve_lambda.py
+++ lambda/part1/Resolve_lambda.py
@@ -30,46 +30,47 @@ b1, b2, b3, b4 = A.row(1)[0], A.row(1)[1], A.row(1)[2], A.row(1)[3]
 c1, c2, c3, c4 = A.row(2)[0], A.row(2)[1], A.row(2)[2], A.row(2)[3]
 d1, d2, d3, d4 = A.row(3)[0], A.row(3)[1], A.row(3)[2], A.row(3)[3]
 
-m11 = remove_required_and_above_smallness_from_expression(-b4*c3*d2 + b3*c4*d2 + b4*c2*d3 - b2*c4*d3 - b3*c2*d4 + b2*c3*d4, order=5)
+# before order was = 5
+m11 = remove_required_and_above_smallness_from_expression(-b4*c3*d2 + b3*c4*d2 + b4*c2*d3 - b2*c4*d3 - b3*c2*d4 + b2*c3*d4, order=2)
 print("... m11 ...", m11)
-m12 = remove_required_and_above_smallness_from_expression(a4*c3*d2 - a3*c4*d2 - a4*c2*d3 + a2*c4*d3 + a3*c2*d4 - a2*c3*d4, order=5)
+m12 = remove_required_and_above_smallness_from_expression(a4*c3*d2 - a3*c4*d2 - a4*c2*d3 + a2*c4*d3 + a3*c2*d4 - a2*c3*d4, order=2)
 print("... m12 ...", m12)
-m13 = remove_required_and_above_smallness_from_expression(-a4*b3*d2 + a3*b4*d2 + a4*b2*d3 - a2*b4*d3 - a3*b2*d4 + a2*b3*d4, order=5)
+m13 = remove_required_and_above_smallness_from_expression(-a4*b3*d2 + a3*b4*d2 + a4*b2*d3 - a2*b4*d3 - a3*b2*d4 + a2*b3*d4, order=2)
 print("... m13 ...", m13)
-m14 = remove_required_and_above_smallness_from_expression(a4*b3*c2 - a3*b4*c2 - a4*b2*c3 + a2*b4*c3 + a3*b2*c4 - a2*b3*c4, order=5)
+m14 = remove_required_and_above_smallness_from_expression(a4*b3*c2 - a3*b4*c2 - a4*b2*c3 + a2*b4*c3 + a3*b2*c4 - a2*b3*c4, order=2)
 print("... m14 ...", m14)
 
-m21 = remove_required_and_above_smallness_from_expression(b4*c3*d1 - b3*c4*d1 - b4*c1*d3 + b1*c4*d3 + b3*c1*d4 - b1*c3*d4, order=5)
+m21 = remove_required_and_above_smallness_from_expression(b4*c3*d1 - b3*c4*d1 - b4*c1*d3 + b1*c4*d3 + b3*c1*d4 - b1*c3*d4, order=2)
 print("... m21 ...", m21)
-m22 = remove_required_and_above_smallness_from_expression(-a4*c3*d1 + a3*c4*d1 + a4*c1*d3 - a1*c4*d3 - a3*c1*d4 + a1*c3*d4, order=5)
+m22 = remove_required_and_above_smallness_from_expression(-a4*c3*d1 + a3*c4*d1 + a4*c1*d3 - a1*c4*d3 - a3*c1*d4 + a1*c3*d4, order=2)
 print("... m22 ...", m22)
-m23 = remove_required_and_above_smallness_from_expression(a4*b3*d1 - a3*b4*d1 - a4*b1*d3 + a1*b4*d3 + a3*b1*d4 - a1*b3*d4, order=5)
+m23 = remove_required_and_above_smallness_from_expression(a4*b3*d1 - a3*b4*d1 - a4*b1*d3 + a1*b4*d3 + a3*b1*d4 - a1*b3*d4, order=2)
 print("... m23 ...", m23)
-m24 = remove_required_and_above_smallness_from_expression(-a4*b3*c1 + a3*b4*c1 + a4*b1*c3 - a1*b4*c3 - a3*b1*c4 + a1*b3*c4, order=5)
+m24 = remove_required_and_above_smallness_from_expression(-a4*b3*c1 + a3*b4*c1 + a4*b1*c3 - a1*b4*c3 - a3*b1*c4 + a1*b3*c4, order=2)
 print("... m24 ...", m24)
 
-m31 = remove_required_and_above_smallness_from_expression(-b4*c2*d1 + b2*c4*d1 + b4*c1*d2 - b1*c4*d2 - b2*c1*d4 + b1*c2*d4, order=5)
+m31 = remove_required_and_above_smallness_from_expression(-b4*c2*d1 + b2*c4*d1 + b4*c1*d2 - b1*c4*d2 - b2*c1*d4 + b1*c2*d4, order=2)
 print("... m31 ...", m31)
-m32 = remove_required_and_above_smallness_from_expression(a4*c2*d1 - a2*c4*d1 - a4*c1*d2 + a1*c4*d2 + a2*c1*d4 - a1*c2*d4, order=5)
+m32 = remove_required_and_above_smallness_from_expression(a4*c2*d1 - a2*c4*d1 - a4*c1*d2 + a1*c4*d2 + a2*c1*d4 - a1*c2*d4, order=2)
 print("... m32 ...", m32)
-m33 = remove_required_and_above_smallness_from_expression(-a4*b2*d1 + a2*b4*d1 + a4*b1*d2 - a1*b4*d2 - a2*b1*d4 + a1*b2*d4, order=5)
+m33 = remove_required_and_above_smallness_from_expression(-a4*b2*d1 + a2*b4*d1 + a4*b1*d2 - a1*b4*d2 - a2*b1*d4 + a1*b2*d4, order=2)
 print("... m33 ...", m33)
-m34 = remove_required_and_above_smallness_from_expression(a4*b2*c1 - a2*b4*c1 - a4*b1*c2 + a1*b4*c2 + a2*b1*c4 - a1*b2*c4, order=5)
+m34 = remove_required_and_above_smallness_from_expression(a4*b2*c1 - a2*b4*c1 - a4*b1*c2 + a1*b4*c2 + a2*b1*c4 - a1*b2*c4, order=2)
 print("... m34 ...", m34)
 
-m41 = remove_required_and_above_smallness_from_expression(b3*c2*d1 - b2*c3*d1 - b3*c1*d2 + b1*c3*d2 + b2*c1*d3 - b1*c2*d3, order=5)
+m41 = remove_required_and_above_smallness_from_expression(b3*c2*d1 - b2*c3*d1 - b3*c1*d2 + b1*c3*d2 + b2*c1*d3 - b1*c2*d3, order=2)
 print("... m41 ...", m41)
-m42 = remove_required_and_above_smallness_from_expression(-a3*c2*d1 + a2*c3*d1 + a3*c1*d2 - a1*c3*d2 - a2*c1*d3 + a1*c2*d3, order=5)
+m42 = remove_required_and_above_smallness_from_expression(-a3*c2*d1 + a2*c3*d1 + a3*c1*d2 - a1*c3*d2 - a2*c1*d3 + a1*c2*d3, order=2)
 print("... m42 ...", m42)
-m43 = remove_required_and_above_smallness_from_expression(a3*b2*d1 - a2*b3*d1 - a3*b1*d2 + a1*b3*d2 + a2*b1*d3 - a1*b2*d3, order=5)
+m43 = remove_required_and_above_smallness_from_expression(a3*b2*d1 - a2*b3*d1 - a3*b1*d2 + a1*b3*d2 + a2*b1*d3 - a1*b2*d3, order=2)
 print("... m43 ...", m43)
-m44 = remove_required_and_above_smallness_from_expression(-a3*b2*c1 + a2*b3*c1 + a3*b1*c2 - a1*b3*c2 - a2*b1*c3 + a1*b2*c3, order=5)
+m44 = remove_required_and_above_smallness_from_expression(-a3*b2*c1 + a2*b3*c1 + a3*b1*c2 - a1*b3*c2 - a2*b1*c3 + a1*b2*c3, order=2)
 print("... m44 ...", m44)
 
 det = a4*b3*c2*d1 - a3*b4*c2*d1 - a4*b2*c3*d1 + a2*b4*c3*d1 + a3*b2*c4*d1 - a2*b3*c4*d1 - a4*b3*c1*d2 + a3*b4*c1*d2 \
       + a4*b1*c3*d2 - a1*b4*c3*d2 - a3*b1*c4*d2 + a1*b3*c4*d2 + a4*b2*c1*d3 - a2*b4*c1*d3 - a4*b1*c2*d3 + a1*b4*c2*d3 \
       + a2*b1*c4*d3 - a1*b2*c4*d3 - a3*b2*c1*d4 + a2*b3*c1*d4 + a3*b1*c2*d4 - a1*b3*c2*d4 - a2*b1*c3*d4 + a1*b2*c3*d4
-det = remove_required_and_above_smallness_from_expression(det, order=5)
+det = remove_required_and_above_smallness_from_expression(det, order=2)
 print("... det ...", det)
 
 A_semi_inv = Matrix([[m11, m12, m13, m14],
@@ -91,7 +92,7 @@ def solve_transform_and_write_to_file(lambda_i, i):
     bottom = simplify(
         remove_required_and_above_smallness_from_expression(
             expand(λ_i_bottom, deep=True),
-            order=5
+            order=2
         )
     )
     number = bottom.args[0]
@@ -99,7 +100,7 @@ def solve_transform_and_write_to_file(lambda_i, i):
 
     top = expand_and_collect_term_before_derivatives_and_lambda(
         remove_required_and_above_smallness_from_expression(
-            expand(λ_i_top, deep=True)/number, order=5
+            expand(λ_i_top, deep=True)/number, order=2
         )
     )
 
